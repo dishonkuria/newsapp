3857a1f89cfa4429c6394b09b5456faf893b125e
dishonkuria <amster2ent@gmail.com>
2018-01-28 23:21:36 +0300

    add newsarticle.py file contents

---
+++ models/newsarticle.py
@@ -0,0 +1,12 @@
+class Newsarticle:
+    '''
+    newsarticle class  to define news article objects
+    '''
+    def  __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
